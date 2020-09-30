ab9d6c976015118399fe8594743daa2c092c5594
Trideep Barik <trideep.india@gmail.com>
2020-10-01 00:26:09 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ session_start();
 include('config.php');
 $email=$_POST['email'];
 $password=$_POST['password'];
-$login="SELECT * FROM login WHERE email='$email' AND password='$password'";
+$login="SELECT * FROM login WHERE BINARY email='$email' AND BINARY password='$password'";
 $login_result=mysqli_query($con,$login) or die(mysqli_error($con));
 header('Location:attendance.php');
 $_SESSION['email']=$email;
